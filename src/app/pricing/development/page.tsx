8218d440bcae1b25d71005a0f4ab0e75bc3106e6
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2023-09-23 19:46:27 +0700

    Update Price and Logo

--- src/app/pricing/development/page.tsx
+++ src/app/pricing/development/page.tsx
@@ -13,18 +13,25 @@ function Development() {
     </p>
     </div>
     <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+
       <div className="col">
-        <div className="card mb-4 rounded-3 shadow-sm">
-          <div className="card-header py-3">
-            <h4 className="my-0 fw-normal">Sewa</h4>
+      <div className="card mb-4 rounded-3 shadow-sm border-primary">
+          <div className="card-header py-3 text-bg-primary border-primary">
+            <h4 className="my-0 fw-normal">Compro Reguler</h4>
           </div>
           <div className="card-body">
-            <h1 className="card-title pricing-card-title">Start 500K<small className="text-body-secondary fw-light">/Month</small></h1>
+            <h1 className="card-title pricing-card-title">499 ribu<small className="text-body-secondary fw-light">/bulan</small></h1>
             <ul className="list-unstyled mt-3 mb-4">
+              <li>Website Company Profile</li>
               <li>Include Domain</li>
-              <li>Include Hosting</li>
-              <li>Include SSL</li>
-              <li>Include Development Web</li>
+              <li>Include Server Shared</li>
+              <li>Include SSL (HTTPS)</li>
+              <li>Include Design</li>
+              <li>Include Logo Perusahaan</li>
+              <li>Include 5 Email gratis</li>
+              <li>Include Maintenance Content</li>
+              <li>Free Development Web</li>
+              <li>Min Pembayaran 5 bulan di awal</li>
             </ul>
             <a href="https://api.whatsapp.com/send?phone=6285171064406&text=beebeesoft-konsultasi">
                 <button type="button" className="w-100 btn btn-lg btn-outline-primary">Konsultasi</button>
@@ -32,18 +39,46 @@ function Development() {
           </div>
         </div>
       </div>
+
       <div className="col">
         <div className="card mb-4 rounded-3 shadow-sm">
           <div className="card-header py-3">
-            <h4 className="my-0 fw-normal">Beli</h4>
+            <h4 className="my-0 fw-normal">Compro Profesional</h4>
           </div>
           <div className="card-body">
-            <h1 className="card-title pricing-card-title">Start 5Jt<small className="text-body-secondary fw-light">/One Time</small></h1>
+            <h1 className="card-title pricing-card-title">999 ribu<small className="text-body-secondary fw-light">/bulan</small></h1>
             <ul className="list-unstyled mt-3 mb-4">
-                <li>Include Domain 1 Year</li>
+              <li>Website Company Profile</li>
+              <li>Website Ecommerce</li>
+              <li>All in Compro Reguler</li>
+              <li>Digital Marketing</li>
+              <li>Search Engine Optimization</li>
+              <li>Min Pembayaran 3 bulan di awal</li>
+            </ul>
+            <a href="https://api.whatsapp.com/send?phone=6285171064406&text=beebeesoft-konsultasi">
+                <button type="button" className="w-100 btn btn-lg btn-outline-primary">Konsultasi</button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="card mb-4 rounded-3 shadow-sm">
+          <div className="card-header py-3">
+            <h4 className="my-0 fw-normal">Compro Flash</h4>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">5Jt<small className="text-body-secondary fw-light">/One Time</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
+              <li>Website Company Profile</li>
+              <li>Website Ecommerce</li>
+              <li>Include Domain 1 Year</li>
               <li>Include Hosting 1 Year</li>
               <li>Include SSL 1 Year</li>
+              <li>Include Design</li>
               <li>Include Development Web</li>
+              <li>Include Garansi 1 Tahun</li>
+              <li>No Maintenance</li>
             </ul>
             <a href="https://api.whatsapp.com/send?phone=6285171064406&text=beebeesoft-konsultasi">
                 <button type="button" className="w-100 btn btn-lg btn-outline-primary">Konsultasi</button>
@@ -51,7 +86,8 @@ function Development() {
           </div>
         </div>
       </div>
-      <div className="col">
+
+      {/* <div className="col">
         <div className="card mb-4 rounded-3 shadow-sm border-primary">
           <div className="card-header py-3 text-bg-primary border-primary">
             <h4 className="my-0 fw-normal">Enterprise</h4>
@@ -69,7 +105,8 @@ function Development() {
             </a>
           </div>
         </div>
-      </div>
+      </div> */}
+
     </div>
 
     </div>
